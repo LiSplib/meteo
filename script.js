90733d8f5ed3bdbd50366d95f312111725b2fbac
LiSplib <52524271+LiSplib@users.noreply.github.com>
2019-12-30 17:08:52 +0100

    refactoring js

--- script.js
+++ script.js
@@ -2,27 +2,10 @@
 
     const icon = document.getElementById('icon');
     const temp = document.getElementById('city-temp');
+    const tempAll = document.getElementById('cityTempAll');
     const condition = document.getElementById('city-condition');
     const day = document.getElementById('day-time');
-
-    const icon1 = document.getElementById('icond1');
-    const temp1 = document.getElementById('city-tempd1');
-    const temp1M = document.getElementById('city-tempd12');
-    const condition1 = document.getElementById('city-conditiond1');
-    const day1 = document.getElementById('day-timed1');
-
-    const icon2 = document.getElementById('icond2');
-    const temp2 = document.getElementById('city-tempd2');
-    const temp2M = document.getElementById('city-tempd22');
-    const condition2 = document.getElementById('city-conditiond2');
-    const day2 = document.getElementById('day-timed2');
-
-    const icon3 = document.getElementById('icond3');
-    const temp3 = document.getElementById('city-tempd3');
-    const temp3M = document.getElementById('city-tempd32');
-    const condition3 = document.getElementById('city-conditiond3');
-    const day3 = document.getElementById('day-timed3');
-
+    const forecastList= document.getElementById('forecast-list');
 
     function updateWeather(cityName){
             fetch(`https://www.prevision-meteo.ch/services/json/${cityName}`)
@@ -34,33 +17,48 @@
     function updateView(data){
         icon.setAttribute("src", data.current_condition.icon_big);
         temp.textContent = data.current_condition.tmp + "°C";
+        tempAll.textContent = "Min : " + data.fcst_day_0.tmin + "°C" + " " + "Max : " +data.fcst_day_0.tmax + "°C";
         condition.textContent = data.current_condition.condition;
         day.textContent = data.fcst_day_0.day_long + " " + data.fcst_day_0.date + " " + data.current_condition.hour;
 
-        icon1.setAttribute("src", data.fcst_day_1.icon_big);
-        temp1.textContent = "Temp. min" + " " + data.fcst_day_1.tmin + "°C";
-        temp1M.textContent = "Temp. max" + " " + data.fcst_day_1.tmax + "°C";
-        condition1.textContent = data.fcst_day_1.condition;
-        day1.textContent = data.fcst_day_1.day_long + " " + data.fcst_day_1.date;
-
-        icon2.setAttribute("src", data.fcst_day_2.icon_big);
-        temp2.textContent = "Temp. min" + " " + data.fcst_day_2.tmin + "°C";
-        temp2M.textContent = "Temp. max" + " " + data.fcst_day_2.tmax + "°C";
-        condition2.textContent = data.fcst_day_2.condition;
-        day2.textContent = data.fcst_day_2.day_long + " " + data.fcst_day_2.date;
-
-        icon3.setAttribute("src", data.fcst_day_3.icon_big);
-        temp3.textContent = "Temp. min" + " " + data.fcst_day_3.tmin + "°C";
-        temp3M.textContent = "Temp. max" + " " + data.fcst_day_3.tmax + "°C";
-        condition3.textContent = data.fcst_day_3.condition;
-        day3.textContent = data.fcst_day_3.day_long + " " + data.fcst_day_3.date;
-
         for(city of cityTitles){
             city.textContent = data.city_info.name; 
+        }
+
+        updateForecastView(data);
+    }
+
+    function updateForecastView(data){
+        let html = "";
+
+        for(let dayNum = 1; dayNum <= 4; dayNum++){
+            html += createForecastItemHtml(data, dayNum);
+        }
+        forecastList.innerHTML = html;
+    }
+
+    function createForecastItemHtml(data, dayNum){
+        const dayKey = "fcst_day_" + dayNum;
+        const dayData = data[dayKey];
+
+        return `
+        <div class="carousel-item">
+            <div class="card d-block w-100 bg-dark text-white text-center">
+                <img id ="icond2" src="${dayData.icon}" class="card-img-top" alt="condition météo">
+                    <p id="city-tempd2" class="text-center">Temp mini : ${dayData.tmin} °C</p>
+                    <p id="city-tempd22" class="text-center">Temp max : ${dayData.tmax} °C</p>
+                <div class="card-body">
+                    <h3 class="card-title text-center">${data.city_info.name}</h3>
+                    <p id="city-conditiond2" class="card-text text-center">${dayData.condition}</p>
+                    <p id="day-timed2" class="text-center">${dayData.day_long} ${dayData.date}</p>
+                </div>
+            </div>
+        </div>
+        `;
+    }
 
         
-        }
-    }
+    
 
     function handleError(err){
         console.error(err);
