b95a975d2a793b462291f261025336aff2229671
LiSplib <jcbarret@outlook.fr>
2020-01-08 16:42:18 +0100

    fix bug first day

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-    const cityTitles = document.querySelectorAll('h3');
+    // const cityTitles = document.querySelectorAll('h3');
 
-    const icon = document.getElementById('icon');
-    const temp = document.getElementById('city-temp');
-    const tempAll = document.getElementById('cityTempAll');
-    const condition = document.getElementById('city-condition');
-    const day = document.getElementById('day-time');
+    // const icon = document.getElementById('icon');
+    // const temp = document.getElementById('city-temp');
+    // const tempAll = document.getElementById('cityTempAll');
+    // const condition = document.getElementById('city-condition');
+    // const day = document.getElementById('day-time');
     const forecastList= document.getElementById('forecast-list');
 
     function updateWeather(cityName){
@@ -13,23 +13,44 @@
             .then(data => updateView(data))
             .catch(err => handleError(err));
     }
+    let html = "";
 
     function updateView(data){
-        icon.setAttribute("src", data.current_condition.icon_big);
-        temp.textContent = data.current_condition.tmp + "°C";
-        tempAll.textContent = "Min : " + data.fcst_day_0.tmin + "°C" + " " + "Max : " +data.fcst_day_0.tmax + "°C";
-        condition.textContent = data.current_condition.condition;
-        day.textContent = data.fcst_day_0.day_long + " " + data.fcst_day_0.date + " " + data.current_condition.hour;
+        
 
-        for(city of cityTitles){
-            city.textContent = data.city_info.name; 
-        }
+        // for(city of cityTitles){
+        //     city.textContent = data.city_info.name; 
+        // };
 
+        html += updateCurrentView(data);
         updateForecastView(data);
+
+    }
+
+    function updateCurrentView(data){
+        return `<div class="carousel-item active d-flex">
+        <div class="card d-block w-100 bg-dark text-white text-center">
+            <img id ="icon" src="${data.current_condition.icon_big}" class="card-img-top" alt="condition météo">
+                <p id="city-temp" class="text-center">${data.current_condition.tmp}</p>
+                <p id="cityTempAll" class="text-center">Min ${data.fcst_day_0.tmin}°C \t Max ${data.fcst_day_0.tmax}°C</p>
+            <div class="card-body">
+                <h3 class="card-title text-center">${data.city_info.name}</h3>
+                <p id="city-condition" class="card-text text-center">${data.current_condition.condition}</p>
+                <p id="day-time" class="text-center">${data.fcst_day_0.day_long} ${data.current_condition.date}</p>
+            </div>
+        </div> 
+      </div>`;
+        // icon.setAttribute("src", data.current_condition.icon_big);
+        // temp.textContent = data.current_condition.tmp + "°C";
+        // tempAll.textContent = "Min : " + data.fcst_day_0.tmin + "°C" + " " + "Max : " +data.fcst_day_0.tmax + "°C";
+        // condition.textContent = data.current_condition.condition;
+        // day.textContent = data.fcst_day_0.day_long + " " + data.fcst_day_0.date + " " + data.current_condition.hour;
+        
+
     }
 
     function updateForecastView(data){
-        let html = "";
+        
 
         for(let dayNum = 1; dayNum <= 4; dayNum++){
             html += createForecastItemHtml(data, dayNum);
