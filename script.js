2503e531aafab7203426e5a9d21a9f36454425af
LiSplib <jcbarret@oulook.fr>
2020-01-11 22:06:50 +0100

    fixing style

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
     const forecastList = document.getElementById('forecast-list');
     const hourCondition = document.getElementById('hourCondition');
+    const dayInfo = document.getElementById('dayInfo');
+
 
     function updateWeather(myCity){
             fetch(`https://www.prevision-meteo.ch/services/json/${myCity}`)
@@ -11,8 +13,6 @@
 
     function updateView(data){
         let html = "";
-        // const hourData = [];
-        // hourData.push(data.fcst_day_0.hourly_data);
         html += updateCurrentView(data);
         html += updateForecastView(data);
         forecastList.innerHTML = html;
@@ -23,22 +23,24 @@
         return `
         <div class="carousel-item active d-flex">
             <div class="card d-block w-100 bg-dark text-white text-center">
+                <h3 class="card-title text-center">${data.city_info.name}</h3>
+                <p id="city-condition" class="card-text text-center">${data.current_condition.condition}</p>
                 <img src="${data.current_condition.icon_big}" class="card-img-top icon" alt="condition météo">
-                    <p id="city-condition" class="card-text text-center">${data.current_condition.condition}</p>
                 <div class="card-body">
-                    <h3 class="card-title text-center">${data.city_info.name}</h3>
                     <p id="city-temp" class="text-center">${data.current_condition.tmp}°C</p>
-                    <p id="cityTempAll" class="text-center">Min ${data.fcst_day_0.tmin}°C \t Max ${data.fcst_day_0.tmax}°C</p>
+                    <p id="cityTempAll" class="text-center">Mini : ${data.fcst_day_0.tmin}°C \t Maxi : ${data.fcst_day_0.tmax}°C</p>
                     <p id="day-time" class="text-center">${data.fcst_day_0.day_long} ${data.current_condition.date}</p>
                 </div>
             </div> 
-        </div>`;
+        </div>
+        `;
     }
 
 
 
     function updateHourCondition(data){
-        let html="";
+        let html = "";
+        dayInfo.innerHTML = `Votre journée du ${data.fcst_day_0.day_long} ${data.current_condition.date}`;
         let actualHour = new Date();
         
         for(let hourNum = actualHour.getHours(); hourNum < 24; hourNum++){
@@ -77,10 +79,10 @@
         return `
         <div class="carousel-item">
             <div class="card d-block w-100 bg-dark text-white text-center">
-                <img src="${dayData.icon}" class="card-img-top icon" alt="condition météo">
+                <h3 class="card-title text-center">${data.city_info.name}</h3>
                 <p id="city-conditiond2" class="card-text text-center">${dayData.condition}</p>
+                <img src="${dayData.icon_big}" class="card-img-top icon" alt="condition météo">
                 <div class="card-body">
-                    <h3 class="card-title text-center">${data.city_info.name}</h3>
                     <p id="city-tempd2" class="text-center">Temp mini : ${dayData.tmin} °C</p>
                     <p id="city-tempd22" class="text-center">Temp max : ${dayData.tmax} °C</p>
                     <p id="day-timed2" class="text-center">${dayData.day_long} ${dayData.date}</p>
@@ -107,7 +109,6 @@
     });
 
     function refreshCity(myCity){
-        //html = "";
         updateWeather(myCity);
     }
 
